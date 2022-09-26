ebde1fcf1679300d3dbfbac286ae01cd3ce82f00
Bruno_Santos <ossbruno@outlook.com>
2022-09-26 11:55:26 -0300

    new endpoints

--- src/API.Application/Controllers/UsersController.cs
+++ src/API.Application/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using System.Threading.Tasks;
+using API.Domain.entities;
 using API.Domain.interfaces.Services.User;
 using Microsoft.AspNetCore.Mvc;
 
@@ -42,6 +43,63 @@ namespace API.Application.Controllers
             try
             {
                 return Ok(await _service.Get(id));
+            }
+            catch(ArgumentException ex)
+            {
+                return StatusCode((int) HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateUser(UserEntity user)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                var result = await _service.Post(user);
+
+                if(result != null)
+                    return Created(new Uri(Url.Link("GetById", new {id = result.Id})), result);
+
+                return BadRequest();
+            }
+            catch(ArgumentException ex)
+            {
+                return StatusCode((int) HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(UserEntity user)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                var result = await _service.Put(user);
+
+                if(result != null)
+                    return Ok(result);
+
+                return BadRequest();
+            }
+            catch(ArgumentException ex)
+            {
+                return StatusCode((int) HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpDelete ("{id}")]
+        public async Task<ActionResult> DeleteUser(Guid id)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+            try
+            {
+                return Ok(await _service.Delete(id));
             }
             catch(ArgumentException ex)
             {
