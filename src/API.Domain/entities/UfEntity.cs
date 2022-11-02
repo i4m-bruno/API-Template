e3309af9bfcc1d3fb2a6a33e7ee62b31a747b0c8
Bruno_Santos <ossbruno@outlook.com>
2022-11-02 18:57:11 -0300

    entidades de endereço

---
+++ src/API.Domain/entities/UfEntity.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Domain.entities
+{
+    public class UfEntity : BaseEntity
+    {
+        [Required]
+        [MaxLength(3)]
+        public string Sigla { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Nome { get; set; }
+        public IEnumerable<MunicipioEntity> Municipios { get; set; }
+
+
+    }
+}
