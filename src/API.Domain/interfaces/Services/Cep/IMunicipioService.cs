ce86335aa85142bb9110984aa306b02a427f81eb
Bruno_Santos <ossbruno@outlook.com>
2022-11-03 10:34:36 -0300

    cep models

--- src/API.Domain/interfaces/Services/Cep/IMunicipioService.cs
+++ src/API.Domain/interfaces/Services/Cep/IMunicipioService.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using API.Domain.Dtos.UF;
 
@@ -9,7 +8,7 @@ namespace API.Domain.interfaces.Services.Cep
     public interface IMunicipioService
     {
         Task<MunicipioDto> GetMunicipio(Guid municipioId);
-        Task<MunicipioDto> GetMunicipioByIBGE(string codIBGE);
+        Task<MunicipioDto> GetMunicipioByIBGE(int codIBGE);
         Task<IEnumerable<MunicipioDto>> ListMunicipios();
         Task<MunicipioDtoCreateResult> CreateMunicipio(MunicipioDtoCreate municipioDto);
         Task<MunicipioDtoUpdateResult> UpdateMunicipio(MunicipioDtoUpdate municipioDto);
