47f5a5bae93d78ef9e674fffaa63793bf5224fbe
Bruno_Santos <ossbruno@outlook.com>
2022-11-07 20:55:55 -0300

    testes unitarios de UF

---
+++ src/Api.Data.Test/UfGets.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Data.Test;
+using API.Data.Context;
+using API.Data.Implementations.Cep;
+using API.Domain.entities;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace API.Data.Test
+{
+    public class UfGets  : BaseTest , IClassFixture<DbTeste>
+    {
+        private ServiceProvider _serviceProvider;
+        public UfGets(DbTeste dbTeste)
+        {
+            _serviceProvider = dbTeste.serviceProvider;
+        }
+
+        [Fact]
+        [Trait("Gets","UfEntity")]
+        public async Task E_Possivel_Realizar_Gets()
+        {
+            using(var context = _serviceProvider.GetService<MyContext>())
+            {
+                UFImplementation _repositorio = new UFImplementation(context);
+                UfEntity _entity = new UfEntity()
+                {
+                    Id = new Guid("971dcb34-86ea-4f92-989d-064f749e23c9"),
+                    Sigla = "TO",
+                    Nome = "Tocantins"
+                };
+
+                var existe = _repositorio.ExistAsync(_entity.Id);
+                Assert.True(existe.Result);
+
+                var registro = _repositorio.SelectAsync(_entity.Id);
+                Assert.NotNull(registro);
+                Assert.Equal(registro.Result.Id, _entity.Id);
+                Assert.Equal(registro.Result.Sigla, _entity.Sigla);
+                Assert.Equal(registro.Result.Nome, _entity.Nome);
+
+                var todosRegistros = _repositorio.SelectAsync();
+                Assert.NotNull(todosRegistros);
+                Assert.True(todosRegistros.Result.Count() > 25);
+            }
+        }
+    }
+}
